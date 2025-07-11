557eff4f295ea7859c603d2eda7f81d9cfcb4aa5
eanna durham <eannadurham@gmail.com>
2025-07-11 15:55:37 +0100

    Add deploy workflow with html

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,6 @@
 def test_get_home(web_client):
     response = web_client.get("/")
+    html = response.data.decode("utf-8")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello, world!"
+    assert "Amputee Football World Cup 2018 – Mexico" in html
+
